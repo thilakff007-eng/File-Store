c57bb2695a66f35de5927b769fac4ed482ddf9d3
thilakff007-eng <thilakff007@gmail.com>
2026-04-27 09:39:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -66,4 +66,8 @@ async def runner():
         web_app()
     )
 
-asyncio.run(runner())
+import asyncio
+
+loop = asyncio.new_event_loop()
+asyncio.set_event_loop(loop)
+loop.run_until_complete(runner())
